f604f5a1cd8c372f43715ede4844f963b33247e0
Oskar Risberg <oskar.risberg@stendahls.se>
2020-10-13 12:10:18 +0200

    fix indentation

--- providers/libravatar-org.js
+++ providers/libravatar-org.js
@@ -3,21 +3,21 @@ const got = require('got');
 const cache = new Map();
 
 module.exports = async (emailHash, targetSize, response) => {
-  const libravatarUrl = `https://cdn.libravatar.org/avatar/${ emailHash }?s=${ targetSize }&d=404`;
-
-  if (cache.has(libravatarUrl)) {
-    return  cache.get(libravatarUrl);
-  }
-
-  try {
-    const { body } = await got(libravatarUrl, {
-      encoding: null,
-    });
-
-    cache.set(libravatarUrl, body);
-
-    return body;
-  } catch (error) {
-    return false;
-  }
-}
+    const libravatarUrl = `https://cdn.libravatar.org/avatar/${ emailHash }?s=${ targetSize }&d=404`;
+    
+    if (cache.has(libravatarUrl)) {
+        return cache.get(libravatarUrl);
+    }
+    
+    try {
+        const { body } = await got(libravatarUrl, {
+            encoding: null,
+        });
+        
+        cache.set(libravatarUrl, body);
+        
+        return body;
+    } catch (error) {
+        return false;
+    }
+};
