7574ea80b73baee1287ea4850055ed9c897f7be2
Oskar Risberg <oskar.risberg@stendahls.se>
2020-10-13 12:10:56 +0200

    fix indent

--- modules/silhouetteImage/index.js
+++ modules/silhouetteImage/index.js
@@ -4,17 +4,24 @@ const sharp = require( 'sharp' );
 const cache = new Map();
 
 module.exports = {
-  get: async (size) => {
-    if (cache.has(size)) {
-      return cache.get(size);
+    get: async (size) => {
+        if (cache.has(size)) {
+            return cache.get(size);
+        }
+        
+        console.time('loading-silhouette');
+        let image;
+        try {
+            image = await sharp( path.join( __dirname, 'silhouette.jpg' ) )
+                .resize( size, size )
+                .toBuffer();
+        } catch (loadSilhouetteError){
+            console.log(loadSilhouetteError);
+        }
+        console.timeEnd('loading-silhouette');
+        
+        cache.set(size, image);
+        
+        return image;
     }
-
-    const image = await sharp( path.join( __dirname, 'silhouette.jpg' ) )
-      .resize( size, size )
-      .toBuffer();
-
-    cache.set(size, image);
-
-    return image;
-  }
 }
